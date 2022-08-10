54fccc1497a90bd1f5c600831bd9b3988f21228b
IgnasKr <ignas.krivas@elintarobotics.lt>
2022-08-10 09:17:32 +0300

    feature

--- FilesBackupApp/Program.cs
+++ FilesBackupApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -10,14 +12,10 @@ namespace FilesBackupApp
 {
     internal class Program
     {
+      
         static void Main(string[] args)
         {
-            if (args.Length != 2)
-            {
-
-                Console.WriteLine("Deklaruoti aplankalus. Parametrai:\n 1 - Archivuojamas aplankalas 2 - Saugojimo vieta.");
-            }
-            else
+            if (args.Length == 2)
             {
                 string inputFolder = @args[1];
                 string outputFolder = @args[0];
@@ -31,12 +29,17 @@ namespace FilesBackupApp
                                          DateTime.Now.ToString("HH-mm");
                     FastZip zip = new ICSharpCode.SharpZipLib.Zip.FastZip();
                     zip.CreateEmptyDirectories = true;
-                    zip.CreateZip(inputFolder + "/" + dateAndTime + ".zip", outputFolder, true, "");
+                    zip.CreateZip(inputFolder + "\\" + dateAndTime + ".zip", outputFolder, true, "");
                     if (File.Exists(inputFolder + "/" + dateAndTime + ".zip"))
                         Console.WriteLine("Užbaigta.");
                     else
                         Console.WriteLine("Nepavyko.");
                 }
+            }
+            else
+            {
+
+                Console.WriteLine("Deklaruoti aplankalus. Parametrai:\n 1 - Archivuojamas aplankalas 2 - Saugojimo vieta.");
             }
         }
     }
